10e3313a615d2fbaf678c8b2390c5662555a5bf2
Joaquin Santarcangelo <joaquinsantarcangelo@gmail.com>
2021-03-19 11:52:09 -0300

    Login structure and styling, responsive Home

--- src/components/containers/Home/index.js
+++ src/components/containers/Home/index.js
@@ -1,20 +1,37 @@
 import React, { useState } from "react";
 
+//Icons
+import { BiListPlus } from "react-icons/bi";
+
 const Home = () => {
   const [items, setItems] = useState([
-    { title: "Title", amount: 255, type: "Taxes" },
-    { title: "Title", amount: -55, type: "Fun" },
+    { title: "Title", amount: 255.2, type: "Taxes" },
+    { title: "Title", amount: -55.6, type: "Fun" },
+    { title: "Title", amount: 255.2, type: "Taxes" },
+    { title: "Title", amount: -55.6, type: "Fun" },
+    { title: "Title", amount: 255.2, type: "Taxes" },
+    { title: "Title", amount: -55.6, type: "Fun" },
+    { title: "Title", amount: 255.2, type: "Taxes" },
+    { title: "Title", amount: -55.6, type: "Fun" },
+    { title: "Title", amount: 255.2, type: "Taxes" },
+    { title: "Title", amount: -55.6, type: "Fun" },
   ]);
-  const [balance, setBalance] = useState(100);
+  const [balance, setBalance] = useState(100.55);
 
   return (
     <div className="home">
+      <div className="add-button">
+        <BiListPlus />
+      </div>
       <div className="container">
         <div className="balance">
-          <div className="text">Your balance:</div>
+          <div className="text">
+            Hi Joaquin! <br /> <span>Your current balance:</span>
+          </div>
           <div className="amount">${balance}</div>
         </div>
         <div className="items">
+          <h1 className="title">Last Transactions</h1>
           {items.map((item) => (
             <div className="item">
               <div className="wrapper">
--- src/components/containers/Login/index.js
+++ src/components/containers/Login/index.js
@@ -4,7 +4,12 @@ const index = () => {
   return (
     <div className="login">
       <div className="card">
-        
+        <h1>Login</h1>
+        <div className="wrapper">
+          <input type="email" name="email" id="email" placeholder="Email adress" />
+          <input type="password" name="" id="" placeholder="Password" />
+          <button>Log in</button>
+        </div>
       </div>
     </div>
   );
